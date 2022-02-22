8cd5e522002d21b6478ad3f681974020d6d16659
Luke Strutz <lstrutz@live.com>
2022-02-21 19:07:55 -0700

    SQL queries under view and an update dept html

---
+++ views/queries.sql
@@ -0,0 +1,16 @@
+-- Department Page
+
+-- Main Department table
+
+-- Department Read operation
+SELECT departmentID, deparmentName, capacity 
+FROM Departments;
+
+-- Department Create operation from input fields.
+INSERT INTO Departments (deparmentName, capacity)
+VALUES (:deparmentNameInput, :capacityInput);
+
+-- Department Update operation.  
+UPDATE Departments SET deparmentName=:deparmentNameInput, capacity=:capacityInput WHERE departmentNameID = :departmentIDForm
+
+-- Department Delete operation
