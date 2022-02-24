3f1ff65bd334344c93ee7e4122438dfcd705f1e0
Lucas Strutz <strutzl@flip2.engr.oregonstate.edu>
2022-02-24 08:04:07 -0800

    added layouts folder and hbs files

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ const app = express();
 
 app.use(express.static('public'));
 
-// app.get("/", function(req,res){
-//     res.send("Hello World!");
-// })
+const { engine } = require('express-handlebars'); // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+app.get('/handle', function(req, res)
+    {
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
 //Listening
 app.listen(PORT, () => {
