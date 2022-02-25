5e7698178d6acdf21c62ec3f463df1cbaf0d69d6
Lucas Strutz <strutzl@flip2.engr.oregonstate.edu>
2022-02-25 13:26:54 -0800

    connect medications anchor to app.get

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ const { engine } = require('express-handlebars'); // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
-app.get('/handle', function(req, res)
+app.get('/medications', function(req, res)
     {
         let query1 = "SELECT * FROM Medications;";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+            res.render('medications', {data: rows});                  // Render the index.hbs file, and also send the renderer
         });                      
     });                                         
 
