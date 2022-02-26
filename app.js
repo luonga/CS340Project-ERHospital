3745dfdd0a4c540e1f88e8086062a268bffc14d0
Lucas Strutz <strutzl@flip2.engr.oregonstate.edu>
2022-02-25 22:29:11 -0800

    Finally connected hbs and css

--- app.js
+++ app.js
@@ -8,20 +8,26 @@ const PORT = 2324;
 
 const express = require("express");
 const app = express();
-const db = require('./database/db-connector')
+const db = require('./database/db-connector');
+const path = require('path');
 
-app.use(express.static('public'));
-
-const { engine } = require('express-handlebars'); // Import express-handlebars
+const {engine} = require('express-handlebars'); // Import express-handlebars
 
 app.set('view engine', '.hbs'); // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 app.engine('.hbs', engine({
-    extname: ".hbs", 
-    layoutsDir: `${__dirname}/views/layouts`
+    extname: ".hbs",
+    defaultLayout: 'main'
 }));  // Create an instance of the handlebars engine to process templates
 
-app.get('/handle', function(req, res) {
-    res.render('index');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    let query1 = "SELECT * FROM Medications;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {title: 'Handle', data: rows});                  // Render the index.hbs file, and also send the renderer
+        });  
 });
 
 app.get('/medications', function(req, res)
@@ -30,7 +36,7 @@ app.get('/medications', function(req, res)
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-            res.render('main', {data: rows, layoutsDir:'medications.hbs'});                  // Render the index.hbs file, and also send the renderer
+            res.render('medications', {title: 'Medications', data: rows});                  // Render the index.hbs file, and also send the renderer
         });                      
     });   
 
@@ -40,7 +46,7 @@ app.get('/doctors', function(req, res)
 
     db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-        res.render('doctors', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        res.render('doctors', {title: 'Doctors', data: rows});                  // Render the index.hbs file, and also send the renderer
     });                      
 });
 
@@ -50,7 +56,7 @@ app.get('/departments', function(req, res)
 
     db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-        res.render('departments', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        res.render('departments', {title: 'Departments', data: rows});                  // Render the index.hbs file, and also send the renderer
     });                      
 });
 
@@ -60,7 +66,7 @@ app.get('/patients', function(req, res)
 
     db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-        res.render('patients', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        res.render('patients', {title: 'Patients',data: rows});                  // Render the index.hbs file, and also send the renderer
     });                      
 });
 
@@ -70,7 +76,7 @@ app.get('/medpatients', function(req, res)
 
     db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-        res.render('medpatients', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        res.render('medpatients', {title: 'Med-Patients',data: rows});                  // Render the index.hbs file, and also send the renderer
     });                      
 });
 
