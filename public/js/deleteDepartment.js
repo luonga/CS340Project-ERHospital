4b10b8f3ac3f2823032d090b2b770fe529e4bfe7
Luke Strutz <lstrutz@live.com>
2022-03-04 08:30:08 -0700

    matching deleteDepartment

--- public/js/deleteDepartment.js
+++ public/js/deleteDepartment.js
@@ -10,21 +10,3 @@ function deleteDepartment(departmentID) {
     }
   })
 }
-
-
-
-// 'use strict';
-
-// function deleteDepartment(departmentID){
-//   console.log('You made it to ajax');
-//   $.ajax({
-//       url: '/handle/' + departmentID,
-//       method: 'DELETE',
-//       success: function(result){
-//           window.location.reload(true);
-//       }, 
-//       error: function(result, textStatus) {
-//         alert(textStatus);
-//       }
-//   })
-// };
